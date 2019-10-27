a57a7d8e03eca70f9de2941ad54edfdd69c7e0ea
Yeongmin Kim <oinochoe@naver.com>
2019-10-27 16:18:53 +0900

    웹팩으로 babel과 scss를 사용한다

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// index.js
+import _ from "lodash";
+import "../scss/style.scss";
+
+function component() {
+  const dragon = document.createElement("div");
+
+  /* lodash is required for the next line to work */
+  dragon.innerHTML = _.join(["Hello", "webpack"], " ");
+
+  return dragon;
+}
+
+document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require("path");
+
+module.exports = {
+  mode: "none",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["css-loader"]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader"
+        ]
+      }
+    ]
+  }
+};
