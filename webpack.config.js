2864d07d83535eba8a998b0463a837e0c7391942
Yeongmin Kim <oinochoe@naver.com>
2019-10-27 16:30:35 +0900

    webpack dev 서버 설치 및 해당 html packing

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "none",
@@ -7,6 +8,15 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
   },
+  devServer: {
+    port: 9000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // index.html 템플릿을 기반으로 빌드 결과물을 추가해줌
+      template: "index.html"
+    })
+  ],
   module: {
     rules: [
       {
