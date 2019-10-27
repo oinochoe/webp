217c3967855c071d91e0c67d65979673c20d7fc8
Yeongmin Kim <oinochoe@naver.com>
2019-10-27 17:11:34 +0900

    sass 테스트 진행중

--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
 // index.js
 import _ from "lodash";
-import Scroll from "../components/Scroll.js";
-import "../scss/style.scss";
 import "../scss/base.scss";
-
-function component() {
-  const dragon = document.createElement("div");
-
-  /* lodash is required for the next line to work */
-  dragon.innerHTML = _.join(["Hello", "webpack"], " ");
-  return dragon;
-}
-
-document.body.appendChild(component());
+import "../scss/style.scss";
